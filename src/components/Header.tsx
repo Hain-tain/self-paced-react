38d63a52432afd4b0db7219a9704c752e7c392d9
hain-tain <hain7ce@gmail.com>
2024-04-23 20:14:41 +0900

    feat: header 음식점 추가 버튼 이미지 적용

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,13 +1,14 @@
 import "./Header.css";
 
 import React from "react";
+import addButton from "../assets/add-button.png";
 
 export default function Header() {
   return (
     <header className="gnb">
       <h1 className="gnb__title text-title">점심 뭐 먹지</h1>
       <button type="button" className="gnb__button" aria-label="음식점 추가">
-        <img src="./add-button.png" alt="음식점 추가" />
+        <img src={addButton} alt="음식점 추가" />
       </button>
     </header>
   );
